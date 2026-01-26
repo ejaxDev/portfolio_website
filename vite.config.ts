7f65645c135b4245cd05b5e211543501f6196932
Eric Jackson <koochieking24@gmail.com>
2026-01-26 14:13:09 -0500

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio_website/',  // <-- Add this to fix asset paths
   plugins: [react()],
 })
